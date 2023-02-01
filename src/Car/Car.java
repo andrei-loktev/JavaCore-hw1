a269a35d16b6c82dc59ff23ecc1c63af80812ddf
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2023-02-02 00:47:04 +0300

    домашка выполнена

--- src/Car/Car.java
+++ src/Car/Car.java
@@ -8,13 +8,53 @@ public class Car {
     int year;
     String country;
 
-    public Car (String brand, String model, int year, String country, String color, double engineVolume ) {
+    public Car (String brand, String model, int year, String country, String color, double engineVolume) {
+        this.brand = checkBrand(brand);
+        this.model = checkModel(model);
+        this.year = checkYear(year);
+        this.country = checkCountry(country);
+        this.color = checkColor(color);
+        this.engineVolume = checkEngineVolume(engineVolume);
+    }
+    public String checkBrand(String brand) {
+        if (brand == null || brand == "") {
+            return "default";
+        }
         this.brand = brand;
+        return brand;
+    }
+    public String checkModel(String model) {
+        if (model == null || model == "") {
+            return "default";
+        }
         this.model = model;
-        this.year = year;
+        return model;
+    }
+    public String checkCountry(String country) {
+        if (country == null || country == "") {
+            return "default";
+        }
         this.country = country;
+        return country;
+    }
+    public int checkYear(int year) {
+        if (year <= 0) {
+            year = 2000;
+        }
+        return year;
+    }
+    public String checkColor(String color) {
+        if (color == null || color == "") {
+            return "белый";
+        }
         this.color = color;
-        this.engineVolume = engineVolume;
+        return color;
+    }
+    public double checkEngineVolume(double engineVolume) {
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
+        return engineVolume;
     }
 
     @Override
