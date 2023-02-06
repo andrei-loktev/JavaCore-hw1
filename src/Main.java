b64816b7bbdffb858796d38ff6328fea14b1fb66
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2023-02-07 02:01:48 +0300

    домашка выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
-import Car.Car;
+import transport.Car;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Lada", "Granta", 2015, "Russia", "жёлтый", 1.7);
-        Car car1 = new Car("Audi", "A8 50 L TDI quattro", 2020 , "Germany", "чёрный", 3.0);
-        Car car2 = new Car("Bmw", "Z8", 2021, "Germany", "чёрный", 3.0);
-        Car car3 = new Car("Kia", "Sportage", 2018, "South Korea", "красный", 2.4);
-        Car car4 = new Car("Hyundai", "Avante", 2016, "South Korea", "оранжевый", 1.6);
+        Car car = new Car("Lada", "Granta", 2015, "Russia", "жёлтый",
+                1.7, "МКПП","седан",  null, 5,
+                true, new Car.Key(false, false));
+        Car car1 = new Car("Audi", "A8 50 L TDI quattro", 2020 , "Germany", "чёрный",
+                3.0, "АКПП", "седан", "444", 4,
+                true, new Car.Key(true, true));
+        Car car2 = new Car("Bmw", "Z8", 2021, "Germany", "чёрный",
+                3.0, "РКПП", "кабрик", "222", 2,
+                false, new Car.Key(true, false));
+        Car car3 = new Car("Kia", "Sportage", 2018, "South Korea", "красный",
+                2.4, "АКПП", "кроссовер", "555", 5,
+                false, new Car.Key(false, true));
+        Car car4 = new Car("Hyundai", "Avante", 2016, "South Korea", "оранжевый",
+                1.6, "МКПП", "седан", "999", 4,
+                true, new Car.Key(false, false));
 
         System.out.println(car.toString());
         System.out.println(car1.toString());
---
+++ src/transport/Car.java
@@ -0,0 +1,171 @@
+package transport;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private final int year;
+    private final String country;
+
+    private String gear;
+    private final String bodyType;
+    private String registerNumber;
+    private final int seat;
+    private boolean isSummerTyres;
+    private Key key;
+
+
+    public static class Key {
+        private final boolean startEngine;
+        private final boolean smartKey;
+
+        public Key(boolean startEngine, boolean smartKey) {
+            this.startEngine = startEngine;
+            this.smartKey = smartKey;
+        }
+
+        @Override
+        public String toString() {
+            return (startEngine ? "с автозапуском " : "без автозапуска") + ", " +
+                    (smartKey ? "есть бесключевой доступ " : "нет бесключевого доступа");
+        }
+    }
+
+    public Car(String brand, String model,
+               int year, String country,
+               String color, double engineVolume, String gear,
+               String bodyType, String registerNumber,
+               int seat, boolean isSummerTyres, Key key) {
+        this.brand = checkBrand(brand);
+        this.model = checkModel(model);
+        this.year = checkYear(year);
+        this.country = checkCountry(country);
+        this.color = checkColor(color);
+        this.engineVolume = checkEngineVolume(engineVolume);
+        this.gear = checkGear(gear);
+        this.bodyType = checkBodyType(bodyType);
+        this.seat = seat;
+        this.registerNumber = checkRegNumber(registerNumber);
+        this.isSummerTyres = isSummerTyres;
+        this.key = key;
+
+    }
+    public String getBrand() {return brand;}
+    public String getModel() {return model;}
+    public double getEngineVolume() {return engineVolume;}
+    public String getColor() {return color;}
+    public int getYear() {return year;}
+    public String getCountry() {return country;}
+    public String getGear() {return gear;}
+    public String getBodyType() {return bodyType;}
+    public String getRegisterNumber() {return registerNumber;}
+    public int getSeat() {return seat;}
+
+    public Key getKey() {return key;}
+
+    public void setKey(Key key) {
+        if (key == null){
+            key = new Key(true, true);
+        }
+        this.key = key;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = checkEngineVolume(engineVolume);
+    }
+    public void setColor(String color) {
+        this.color = checkColor(color);
+    }
+    public void setGear(String gear) {
+        this.gear = checkGear(gear);
+    }
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+    public void setSummerTyres(boolean summerTyres) {
+        isSummerTyres = summerTyres;
+    }
+    public String checkRegNumber(String registerNumber){
+        if (registerNumber == null || registerNumber.isEmpty()) {
+            registerNumber = "xxx";
+        }
+        return registerNumber;
+    }
+
+    public void changeTyres(int month) {
+        if ((month >= 1 && month <= 4) || (month >= 11 && month <= 12)) {
+            isSummerTyres = false;
+        }
+        if (month >= 5 && month <= 10) {
+            isSummerTyres = true;
+        }
+    }
+    public String checkBrand(String brand) {
+        if (brand == null || brand.isEmpty()) {
+            return "default";
+        }
+        return brand;
+    }
+    public String checkModel(String model) {
+        if (model == null || model.isEmpty()) {
+            return "default";
+        }
+        return model;
+    }
+    public String checkCountry(String country) {
+        if (country == null || country.isEmpty()) {
+            return "default";
+        }
+        return country;
+    }
+    public int checkYear(int year) {
+        if (year <= 0) {
+            year = 2000;
+        }
+        return year;
+    }
+    public String checkColor(String color) {
+        if (color == null || color.isEmpty()) {
+            return "белый";
+        }
+        return color;
+    }
+    public double checkEngineVolume(double engineVolume) {
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
+        return engineVolume;
+    }
+    public String checkBodyType(String bodyType) {
+        if (bodyType == null || bodyType.isEmpty()) {
+            return "default";
+        }
+        return bodyType;
+    }
+    public String checkGear(String gear) {
+        if (gear == null || gear.isEmpty()) {
+            return "default";
+        }
+        return gear;
+    }
+
+    @Override
+    public String toString() {
+        return "машина " +
+                "марка='" + brand + '\'' +
+                ", модель='" + model + '\'' +
+                ", двигатель=" + engineVolume +
+                ", цвет='" + color + '\'' +
+                ", год=" + year +
+                ", страна='" + country + '\'' +
+                ", коробка='" + gear + '\'' +
+                ", тип кузова='" + bodyType + '\'' +
+                ", госномер='" + registerNumber + '\'' +
+                ", кол-во мест=" + seat + ", " +
+                (isSummerTyres ? "летняя" : "зимняя") + " резина" + ", " +
+                key;
+    }
+}
+
+
